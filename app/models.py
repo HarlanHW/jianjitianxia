e099c8a9c6536029eb74ec202330b6f931a2dda6
HarlanHW <1223101214@qq.com>
2018-03-22 23:52:06 +0800

    20180318 添加了用户基本信息表

--- app/models.py
+++ app/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
+# 说明：所有主键为length为8
+
 # 用户基本信息表
-#包含用户注册的基本信息
+# 包含用户注册的基本信息
 class user(models.Model):
-    user_id = models.CharField(max_length=8,primary_key=True)#用户id，主键长度为8
-    user_name = models.CharField(max_length=16)#用户名，限制16个字符
-    user_password = models.CharField(max_length=16)
-    user_sex = models.CharField(max_length=1,choices=['男',"女","其他"],default="其他")
-    user_email = models.EmailField
+    user_id = models.CharField(max_length=8,primary_key=True) #用户id，主键长度为8
+    user_name = models.CharField(max_length=16) #用户名，限制16个字符
+    user_password = models.CharField(max_length=16) #密码
+    user_sex = models.CharField(max_length=1,choices=[(0,"M"),(1,"W"),(2,"X")],default="X")#性别
+    user_email = models.EmailField #电子邮件地址
+    user_phone = models.CharField(max_length=11) #电话号码
     user_educate_id = models.CharField(max_length=8)
-    user_level_id = models.CharField(max_length=8)
-    user_intr_id = models.CharField(max_length=8)
-    user_status_id = models.CharField(max_length=8)
+    user_level_id = models.CharField(max_length=8) #用户等级信息表的主键
+    user_intr_id = models.CharField(max_length=8) #用户介绍信息的主键
+    user_status_id = models.CharField(max_length=8) #用户状态信息的主键
+    user_company_id = models.CharField(max_length=8) #用户公司信息
 
 class educate(models.Model):
     educate_id=models.CharField(max_length=8)
@@ -24,5 +28,4 @@ class seek(models.Model):
 
 
 class level(models.Model):
-    level_id = models.CharField()
-
+    level_id = models.CharField(max_length=8)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
-
+# 主页
 def home(request):
     return render(request, "index.html")
+
+# 注册功能
+def registe(request):
+    name="1"
+    passwd="1"
+    return 1
+
+# 登陆功能
+def login(request):
+    return 1
 
